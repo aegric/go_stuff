8b7f3fc42c45bc632fb6931b3fb45f212ad34bd8
justin <justin@justin-VirtualBox.(none)>
2017-05-16 23:12:23 -0600

    added index.html and folder, moved webserver

--- webserver.go
+++ webserver.go
@@ -1,22 +1,21 @@
-package main 
+package main
 
-import ( "fmt"
-         "net/http"
-         "strconv"
-         "flag"
-         "time"
-     )
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+)
 
+func main() {
+	port := flag.Int("p", 3001, "port number")
+	flag.Parse()
+	portNum := ":" + strconv.Itoa(*port)
+	fmt.Printf("server start %s - %s \n", portNum, time.Now())
+	served := "/home/justin/go/src/github.com/aegric/template"
+	fs := http.FileServer(http.Dir(served))
+	http.Handle("/", fs)
+	http.ListenAndServe(portNum, nil)
 
-     func main (){
-         port := flag.Int("p",3001,"port number")
-         flag.Parse()
-         port_num := ":" + strconv.Itoa(*port)
-         fmt.Printf("server start %s - %s \n",port_num,time.Now())
-         served := "/home/justin/go/src/webserver/template"
-         fs := http.FileServer(http.Dir(served))
-         http.Handle("/",fs)
-         http.ListenAndServe(port_num,nil)
-
-     }
-
+}
